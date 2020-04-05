16d791ae2ee749391f01be7eb774702a46bffe67
Terrell Ibanez <9453223+ibanezt@users.noreply.github.com>
2020-04-04 20:00:24 -0400

    Add Toolbox

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import { Select, TextInput, Main, Box, Button, Collapsible, Heading, Grommet, Layer, ResponsiveContext, Form, FormField, RadioButtonGroup } from 'grommet';
-import { Google, Windows, Amazon, Satellite, FormClose, Notification } from 'grommet-icons';
+import { Accordion, AccordionPanel, Select, TextInput, Main, Box, Button, Collapsible, Heading, Grommet, Layer, ResponsiveContext, Form, FormField, RadioButtonGroup } from 'grommet';
+import { Database, Troubleshoot, Google, Windows, Amazon, Satellite, Close, Notification } from 'grommet-icons';
 
 const theme = {
   /*
@@ -32,11 +32,11 @@ const AppBar = (props) => (
 );
 const Card = (props) => (
   <Box
+    border='true'
     width='large'
-    flex='shrink'
     elevation='medium'
-    gap='small'
-    pad='small'
+    pad='medium'
+    margin='small'
     {...props}
   >
     <Heading
@@ -62,7 +62,7 @@ const ProviderCard = (props) => (
         />
       </FormField>
       <FormField label='Project ID'>
-        <TextInput placeholder="type here"
+        <TextInput placeholder="none"
           value={props.projectID}
           onChange={(event) => props.setProjectID(event.target.value)}
         />
@@ -75,15 +75,94 @@ const ProviderCard = (props) => (
           onChange={({ option }) => props.setRegion(option)}
         />
       </FormField>
+
     </Form>
   </Card >
 )
 const DatabaseCard = (props) => (
-  <Box>
+  <Card
+    title='Database'
+    {...props}
+  >
 
-  </Box>
+  </Card>
+);
+const AccordionCard = (props) => (
+  <AccordionPanel
+    label={props.title}
+    {...props}
+  >
+    {props.children}
+  </AccordionPanel>
 );
 
+const AddFunctionCard = (props) => (
+  <AccordionCard
+    title='Add a Serverless Function'
+    {...props}
+
+  >
+    <Form>
+      <FormField
+        label='Function Name'
+      >
+        <TextInput placeholder="none"
+          value={props.functionName}
+          onChange={(event) => props.setFunctionName(event.target.value)}
+        />
+      </FormField>
+      <FormField
+        label='Function Language'
+      >
+        <RadioButtonGroup
+          name='FunctionLanguageGroup'
+          options={['Python 3.7', 'Node.js 8', 'Node.js 10']}
+          value={props.functionLanguage}
+          onChange={(event) => props.setFunctionLanguage(event.target.value)}
+        />
+      </FormField>
+      <Button type="submit" primary label="Add Function" />
+    </Form>
+  </AccordionCard>
+);
+
+const AddDatabaseTableCard = (props) => (
+  <AccordionCard
+    title='Add a Database Table'
+    {...props}
+  >
+    <Form>
+      <FormField
+        label='Table Name'
+      >
+        <TextInput placeholder='none'
+          value={props.databaseTableName}
+          onChange={(event) => props.setDatabaseTableName(event.target.value)}
+        />
+      </FormField>
+      <Button type="submit" primary label="Add Database Table" />
+    </Form>
+  </AccordionCard>
+);
+
+const AddStorageBucketCard = (props) => (
+  <AccordionCard
+    title='Add a Storage Bucket'
+    {...props}
+  >
+    <Form>
+      <FormField
+        label='Bucket Name'
+      >
+        <TextInput placeholder='none'
+          value={props.storageBucketName}
+          onChange={(event) => props.setStorageBucketName(event.target.value)}
+        />
+      </FormField>
+      <Button type="submit" primary label="Add Storage Bucket" />
+    </Form>
+  </AccordionCard>
+);
 function App() {
   // Default Configuration
   const defaultProvider = 'none';
@@ -105,8 +184,18 @@ function App() {
   const [functions, setFunctions] = useState(initFunction);
   const [databases, setDatabases] = useState(initDatabases);
 
+  // Default App State
+  const initFunctionName = 'none';
+  const initFunctionLanguage = 'none';
+  const initDatabaseTableName = 'none';
+  const initStorageBucketName = 'none';
+
   // App State
   const [showSidebar, setShowSidebar] = useState(false);
+  const [functionName, setFunctionName] = useState(initFunctionName);
+  const [functionLanguage, setFunctionLanguage] = useState(initFunctionLanguage);
+  const [databaseTableName, setDatabaseTableName] = useState(initDatabaseTableName);
+  const [storageBucketName, setStorageBucketName] = useState(initStorageBucketName);
 
   return (
     <Grommet theme={theme} full themeMode="light">
@@ -115,20 +204,31 @@ function App() {
           <Main>
             <AppBar>
               <Heading level='2' margin='none'><Satellite /> Mission Control</Heading>
-              <Button icon={<Notification />} onClick={() => { setShowSidebar(!showSidebar) }} />
+              <Button icon={<Troubleshoot />} onClick={() => { setShowSidebar(!showSidebar) }} />
             </AppBar>
 
-            <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-              <Box flex align='center' justify='center'>
-                <ProviderCard
-                  provider={provider}
-                  setProvider={setProvider}
-                  projectID={projectID}
-                  setProjectID={setProjectID}
-                  region={region}
-                  setRegion={setRegion}
+            <Box direction='row' fill={true}>
+              <Box flex align='center' overflow='scroll'>
+                <Box flex={false}>
+                  <ProviderCard
+                    provider={provider}
+                    setProvider={setProvider}
+                    projectID={projectID}
+                    setProjectID={setProjectID}
+                    region={region}
+                    setRegion={setRegion}
 
-                />
+                  />
+                  <ProviderCard
+                    provider={provider}
+                    setProvider={setProvider}
+                    projectID={projectID}
+                    setProjectID={setProjectID}
+                    region={region}
+                    setRegion={setRegion}
+
+                  />
+                </Box>
               </Box>
 
               {(!showSidebar || size !== 'small') ? (
@@ -141,7 +241,28 @@ function App() {
                     align='center'
                     justify='center'
                   >
-                    Sidebar
+                    <Heading>Toolbox</Heading>
+                    <Accordion
+                      animate={true}
+                      multiple={true}
+                      margin='small'
+                      flex
+                    >
+                      <AddFunctionCard
+                        functionName={functionName}
+                        setFunctionName={setFunctionName}
+                        functionLanguage={functionLanguage}
+                        setFunctionLanguage={setFunctionLanguage}
+                      />
+                      <AddDatabaseTableCard
+                        databaseTableName={databaseTableName}
+                        setDatabaseTableName={setDatabaseTableName}
+                      />
+                      <AddStorageBucketCard
+                        storageBucketName={storageBucketName}
+                        setStorageBucketName={setStorageBucketName}
+                      />
+                    </Accordion>
                   </Box>
                 </Collapsible>
               ) : (
@@ -154,7 +275,7 @@ function App() {
                       direction='row'
                     >
                       <Button
-                        icon={<FormClose />}
+                        icon={<Close />}
                         onClick={() => setShowSidebar(false)}
                       />
                     </Box>
